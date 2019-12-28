ec2c64715e383b903195122fe6042e0a23953aa0
Matthew Ahrenstein <matt@ahrenstein.com>
2019-12-28 18:58:44 -0500

    Fixing typo in start script

--- SourceCode/customStart.sh
+++ SourceCode/customStart.sh
@@ -6,7 +6,7 @@ echo "Provider: vmware"
 echo "CPUs: 4"
 echo "RAM: 4GB"
 echo "Insecure Registry Subnet: 172.16.148.0/24"
-minikube start minikube start --vm-driver=vmware --memory 4096 --cpus 4 --insecure-registry="172.16.148.0/24"
+minikube start --vm-driver=vmware --memory 4096 --cpus 4 --insecure-registry="172.16.148.0/24"
 
 echo "Running \"minikube status\" to verify IP space for registry and MetalLB"
 minikube status
